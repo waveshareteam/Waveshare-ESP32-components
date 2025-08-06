b30f57c707fe82dc93d496ad3052eb02fac27872
Y1hsiaochunnn <y1hsiaochunnn@qq.com>
2025-08-06 11:27:46 +0800

    Fix parameter issues

--- bsp/esp32_p4_wifi6_touch_lcd_4b/include/bsp/display.h
+++ bsp/esp32_p4_wifi6_touch_lcd_4b/include/bsp/display.h
@@ -22,7 +22,7 @@
 
 #define BSP_LCD_H_RES              (720)
 #define BSP_LCD_V_RES              (720)
-#define BSP_LCD_MIPI_DSI_LANE_BITRATE_MBPS (1000)
+#define BSP_LCD_MIPI_DSI_LANE_BITRATE_MBPS (480)
 
 #define BSP_LCD_MIPI_DSI_LANE_NUM          (2)    // 2 data lanes
 #define BSP_MIPI_DSI_PHY_PWR_LDO_CHAN       (3)  // LDO_VO3 is connected to VDD_MIPI_DPHY
