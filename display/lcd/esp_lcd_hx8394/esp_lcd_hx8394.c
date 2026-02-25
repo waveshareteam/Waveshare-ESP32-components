71820e7f1c7e640b89459e04d2345501e632e2f6
excited-hippo <>
2026-02-25 21:17:49 +0100

    lcd: Release the i2c bus in all cases
    The i2c bus needs to be released or otherwise it cannot
    be used by the application afterwards. Remove the comment
    from the line i2c_bus_delete which it was written to solve
    this problem in the first place. I am guessing that the comment
    in this line was just forgotten.

--- display/lcd/esp_lcd_hx8394/esp_lcd_hx8394.c
+++ display/lcd/esp_lcd_hx8394/esp_lcd_hx8394.c
@@ -128,7 +128,7 @@ esp_err_t esp_lcd_new_panel_hx8394(const esp_lcd_panel_io_handle_t io, const esp
     i2c_bus_write_bytes(i2c0_device1, 0x96, 1, &data);
 
     i2c_bus_device_delete(&i2c0_device1);
-    // i2c_bus_delete(&i2c0_bus);
+    i2c_bus_delete(&i2c0_bus);
 
     vTaskDelay(pdMS_TO_TICKS(1000));
 
